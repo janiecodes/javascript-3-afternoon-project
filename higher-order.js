6a9bc5bd36b281f2e73bf331f157eb95502f78cc
Janie Kim <janiekim@Janies-MacBook-Pro.local>
2020-10-09 19:21:56 -0500

    higher-order complete

--- higher-order.js
+++ higher-order.js
@@ -21,9 +21,16 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+//let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+// let evenNumbers = mixedNumbers.filter(function(element, index, array){
+//   return element % 2 === 0
+// })
 
+// let evenNumbers = mixedNumbers.filter( e => {
+//   return e % 2 === 0
+// })
 
+let evenNumbers = mixedNumbers.filter( e => e % 2 === 0)
 
 ////////// PROBLEM 2 //////////
 
@@ -44,9 +51,12 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+//let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+// let postTaxPrices = prices.map(function(element, index, array){
+//   return element *= 1.07
+// }, 0)
 
-
+let postTaxPrices = prices.map(element => element *= 1.07);
 
 ////////// PROBLEM 3 //////////
 
@@ -63,9 +73,12 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+//let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+// let totalPopulation = populations.reduce(function(acc,curr){
+//   return acc + curr
+// })
 
-
+let totalPopulation = populations.reduce((acc,curr) => acc + curr)
 
 ////////// PROBLEM 4 //////////
 
@@ -89,9 +102,13 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+//let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
+// let myStrongest = monstersInYourPocket.filter(function(element, index, array){
+//   return element.CP > 200
+// })
 
+let myStrongest = monstersInYourPocket.filter(element => element.CP > 200)
 
 ////////// PROBLEM 5 //////////
 
@@ -103,12 +120,18 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax 
+  (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer 
+  should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(function(element, index, array){
+  return element.price * (1 + element.tax)
+})
 
 
+
+//let orderTotals = orders.map(element => element.tax *= 1.07)
 
 ////////// PROBLEM 6 //////////
 
@@ -126,6 +149,14 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.filter(function(element, index, array){
+  return element.owner === 'Bob'
+}).map(function(element, index, array){
+  return element.price
+  }).reduce(function(acc, curr){
+    return acc + curr
+  })
+
+
 
 
